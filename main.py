f6ea32c0edfd0c757f5f669b6c9314fa38c00aa1
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-07 02:52:58 -0400

    count words in text

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def get_book_text(filepath):
     content = f.read()
   return content
 
+def count_words(text):
+  words = text.split()
+  return len(words)
+
 def main():
   path = "books/frankenstein.txt"
-  print(get_book_text(path))
+  book = get_book_text(path)
+  count = count_words(book)
+  print(f"{count} words found in the document")
 
 main()
