ecbf96be5a9c18a6da5399efcc1990148ac14e67
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-07 03:46:33 -0400

    moved funtions and made print_stats

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+from stats import count_words, count_characters, sort_characters
+
 def get_book_text(filepath):
   with open(filepath) as f:
     content = f.read()
   return content
 
-def count_words(text):
-  words = text.split()
-  return len(words)
+def print_stats(path, count, characters):
+  print("============ BOOKBOT ============")
+  print(f"Analysis book found at {path}...")
+  print("----------- Word Count ----------")
+  print(f"Found {count} total words")
+  print("--------- Character Count -------")
+  for char in characters:
+    if char['char'].isalpha():
+      print(f"{char['char']}: {char['amount']}")
+  print("============= END ===============")
 
 def main():
   path = "books/frankenstein.txt"
   book = get_book_text(path)
   count = count_words(book)
-  print(f"{count} words found in the document")
+  characters = count_characters(book)
+  sorted = sort_characters(characters)
+  print_stats(path, count, sorted)
 
 main()
